1de43da94880c511c71435af6b24190f019d8fcc
DESKTOP-8NA5MNL\Santi Di Colantonio <dicolantonio@yahoo.com.ar>
2024-06-04 16:14:51 +0200

    Instalacion de angular material

--- mascotas/src/app/app.module.ts
+++ mascotas/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule, MatDividerModule, MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
