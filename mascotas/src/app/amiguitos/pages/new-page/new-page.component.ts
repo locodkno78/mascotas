40269fc62c63a03fa830bde4e6d31683f342d5c6
DESKTOP-8NA5MNL\Santi Di Colantonio <dicolantonio@yahoo.com.ar>
2024-08-02 21:20:43 -0300

    creacion del formulario para cargar nuevo animal

--- mascotas/src/app/amiguitos/pages/new-page/new-page.component.ts
+++ mascotas/src/app/amiguitos/pages/new-page/new-page.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styleUrls: []
+  styleUrls: ['./new-page.component.css'],
 })
-export class NewPageComponent {
-
-}
+export class NewPageComponent {}
